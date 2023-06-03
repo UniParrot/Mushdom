28290ddfe5e5e74bc0683233f495f5529b86c032
UniParrot <intlparrot@gmail.com>
2023-06-03 12:23:44 +0900

    Updated

--- Mushdom/entity/mushroom.py
+++ Mushdom/entity/mushroom.py
@@ -3,31 +3,51 @@ BLUE = (0, 0, 255)
 class Mushroom(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.surface = pygame.Surface((32, 32))
+        self.ratio = 5
         self.image = pygame.image.load("asset/mushroom.png")
-        self.surface.set_colorkey(BLUE)
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+        self.image = pygame.transform.scale(self.image, (self.width*self.ratio, self.height*self.ratio))
+        self.image.set_colorkey((0,255,0))
+        self.surface = pygame.Surface((self.width*self.ratio, self.height*self.ratio))
         self.surface.blit(self.image, (0, 0))
         self.side = None
+        self.rect = self.surface.get_rect()
 
+    def update(self):
+        for event_ in pygame.event.get():
+            if event_.type == pygame.KEYDOWN:
+                if event_.key == pygame.K_UP:
+                    self.rect.y += 10
+                elif event_.key == pygame.K_DOWN:
+                    self.rect.y -= 10
+                elif event_.key == pygame.K_LEFT:
+                    self.rect.x -= 10
+                elif event_.key == pygame.K_RIGHT:
+                    self.rect.x += 10
 class Spore(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.surface = pygame.Surface((16, 16))
-        self.image_1 = pygame.image.load("asset/spore_1.gif")
-        self.image_2 = pygame.image.load("asset/spore_2.gif")
-        self.image_3 = pygame.image.load("asset/spore_3.gif")
         self.surface.set_colorkey(BLUE)
 if __name__ == "__main__":
     TestMushroom = Mushroom()
+    TestMushroomGroup = pygame.sprite.Group()
+    TestMushroomGroup.add(TestMushroom)
     loop = True
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((720, 480))
     pygame.display.init()
     WHITE = (255, 255, 255)
+
     while loop:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 loop = False
         screen.fill(WHITE)
-        screen.blit(TestMushroom.surface, (0, 0))
+
+        TestMushroom.update()
+        TestMushroomGroup.draw(screen)
+        pygame.display.flip()
         pygame.display.update()
+        clock.tick(60)
--- Mushdom/world/generator.py
+++ Mushdom/world/generator.py
@@ -1,2 +1,50 @@
 import pygame
 import random
+
+class Tree(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("asset/tree.png")
+        self.height = self.image.get_height()
+        self.width = self.image.get_width()
+        self.ratio = 3
+        self.image = pygame.transform.scale(self.image, (self.width*self.ratio, self.height*self.ratio))
+        self.surface = pygame.Surface((self.width*self.ratio, self.height*self.ratio))
+        self.surface.set_colorkey((0,0,0))
+        self.surface.blit(self.image, (0, 0))
+        self.rect = self.surface.get_rect()
+
+class World:
+    def __init__(self):
+        self.width = 500
+        self.height = 500
+        self.tree_amount = 10
+        self.random_distance = 0
+        self.tree_group = pygame.sprite.Group()
+        self.tree_test = None
+        self.random_distance_x = random.randint(0, 300)
+        self.random_distance_y = random.randint(0, 300)
+    def plant_tree(self):
+        for i in range(10):
+            self.random_distance_x = random.randint(0, 300)
+            self.random_distance_y = random.randint(0, 300)
+            self.tree_test = Tree()
+            self.tree_test.rect.x = self.random_distance_x
+            self.tree_test.rect.y = self.random_distance_y
+            self.tree_group.add(self.tree_test)
+        return self.tree_group
+
+if __name__ == "__main__" :
+    loop = True
+    test_world = World()
+    screen = pygame.display.set_mode((300, 300), pygame.RESIZABLE)
+    while loop:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                loop = False
+        screen.fill((255,255,255))
+        tree_group = test_world.plant_tree()
+        if len(tree_group) >= 10 :
+            tree_group.kill()
+        tree_group.draw(screen)
+        pygame.display.update()
