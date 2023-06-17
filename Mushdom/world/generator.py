575117d27d170c690ee443b8a4a3a4a2db327e28
UniParrot <intlparrot@gmail.com>
2023-06-17 12:15:35 +0900

    Updated

--- Mushdom/world/generator.py
+++ Mushdom/world/generator.py
@@ -15,6 +15,7 @@ class Tree(pygame.sprite.Sprite):
         self.rect = self.surface.get_rect()
 
 class World:
+
     def __init__(self, world_width, world_height):
         self.width = world_width
         self.height = world_height
@@ -34,33 +35,85 @@ class World:
             self.tree_group.add(self.tree_test)
         return self.tree_group
 class Area(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, width, height, pos_list:list):
         super().__init__()
         self.mushroom = None
         self.mob_count = None
-        self.width = None
-        self.height = None
-def generate_area(world_width, world_height):
-    rect_point_upper_right = 0
-    rect_point_upper_left = 0
-    rect_point_bottom_right = 0
-    rect_point_upper_left = 0
+        self.pos_x = 0
+        self.pos_y = 0
+        self.width = width
+        self.height = height
+        self.pos_list = pos_list
+        self.surface = pygame.surface.Surface((self.width, self.height))
+        self.rect_inner = pygame.Rect(self.width, self.width, self.width, self.width)
+        self.rect_inner.center = [self.width/2, self.height/ 2]
+        self.image = pygame.image.load("asset/test_tile.png")
+        self.image = pygame.transform.scale(self.image, size=(self.width, self.height))
+        self.rect = self.surface.get_rect()
+        self.rect.topleft = pos_list[0]
+        self.rect.topright = pos_list[1]
+        self.rect.bottomleft = pos_list[2]
+        self.rect.bottomright = pos_list[3]
+def generate_area(world_width: int, world_height: int, ratio: int):
+    rect_point_upper_right = [0, 0]
+    rect_point_upper_left = [0, 0]
+    rect_point_bottom_right = [0, 0]
+    rect_point_bottom_left = [0, 0]
     world_width = int(world_width)
     world_height = int(world_height)
-    area_width = math.gcd(world_width, world_height)
-    area_height = math.gcd(world_width, world_height)
-    area_width_amount = world_width / area_width
-    area_height_amount = world_height / area_height
+    area_width = math.gcd(world_width, world_height) / ratio
+    print("area_width:", area_width)
+    area_height = math.gcd(world_width, world_height) / ratio
+    print("area_height:", area_height)
+    area_width_amount = int(world_width / area_width)
+    print("area_width_amount:" , area_width_amount)
+    area_height_amount = int(world_height / area_height)
+    print("area_height_amount:", area_height_amount)
+    area_list = pygame.sprite.Group()
+    area = [[0, 0], [0, 0], [0, 0], [0, 0]]
+    rect_point_upper_right[0] += area_width
+    print("rect_point_upper_right:", rect_point_upper_right)
+    rect_point_bottom_left[1] += area_height
+    print("rect_point_bottom_left:", rect_point_bottom_left)
+    rect_point_bottom_right[0] += area_width
+    print("rect_point_bottom_right:", rect_point_bottom_right)
+    rect_point_bottom_right[1] += area_height
+    print("rect_point_bottom_right:", rect_point_bottom_right)
+    for i in range(area_height_amount):
+        rect_point_upper_left[0] = 0
+        rect_point_upper_right[0] = 0
+        rect_point_bottom_left[0] = 0
+        rect_point_bottom_right[0] = 0
+        for j in range(area_width_amount):
+            rect_point_upper_left[0] += area_width
+            rect_point_upper_right[0] += area_width
+            rect_point_bottom_left[0] += area_width
+            rect_point_bottom_right[0] += area_width
+            area[0] = rect_point_upper_left
+            area[1] = rect_point_upper_right
+            area[2] = rect_point_bottom_left
+            area[3] = rect_point_bottom_right
+            area_list.add(Area(area_width, area_height, area))
+            print(area)
+        rect_point_upper_left[1] += area_height
+        rect_point_upper_right[1] += area_height
+        rect_point_bottom_left[1] += area_height
+        rect_point_bottom_right[1] += area_height
+
+    print(area_list)
+    return area_list
 if __name__ == "__main__" :
     loop = True
-    test_world = World(1920, 1080)
-    screen = pygame.display.set_mode((300, 300), pygame.RESIZABLE)
-    tree_group = test_world.plant_tree()
+    test_world = World(500, 600)
+    screen = pygame.display.set_mode((500, 600))
+
+    area_list = generate_area(500, 600, 2)
+    print("world_size: 500, 500")
     while loop:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 loop = False
         screen.fill((255,255,255))
+        area_list.draw(screen)
 
-        tree_group.draw(screen)
         pygame.display.update()
